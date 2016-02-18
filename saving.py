3b6cedd5fe8c49db9b142c9ee2185e9db406bc4a
david <cyber_veio@hotmail.com>
2016-02-18 11:47:58 +0000

    Loader object rebuilt, with sanity checks on Ascii

--- saving.py
+++ saving.py
@@ -1,65 +1,60 @@
 #For loading and saving data
-def loaddata(filename,windows=False):
-	def loadascii(filename):
-		print "loading data from %s as ascii" %filename
-		starttime = time()
-		import subprocess
-		size = subprocess.check_output(['wc','-l',filename])
-		size = size.split(' ')
-		X = zeros(int(size[0])-5)
-		Y = zeros(int(size[0])-5)
-		f = open(filename)
-		i=0
-		with open(filename) as f:
-			while f.readlines()
-			for line in f:
-				x,y = line.strip().split(',')
-				X[i] = x
-				Y[i] = y
-				i+=1
-			while i < len(X):
-				X[i] = None
-				Y[i] = None
-				i+=1
-		endtime = time()
-		print "That took %i seconds" %int(endtime - starttime)
-		return X,Y
+class loader:
+	def loaddata(self,filename,col=2):
+		def isfloat(linedata):
+			floatable = True
+			for value in linedata:
+				try:
+					float(value)
+				except ValueError:
+					floatable = False
+			return floatable
 
-	def loadraw(filename):
-		startime = time()
-		import JLeCroy
-		print "Loading data from %s" %filename
-		f = open(filename,'rb')
-		raw = f.read()
-		wave,t,x,Int = JLeCroy.InterpretWaveform(raw)
-		endtime = time()
-		print "That took %i seconds" %int(endtime - startime)
-		return t,x
+		def loadascii(filename):
+			print "Loading data from %s as ascii" %filename
+			starttime = time()
 
-	def winload(filename):
-		from pylab import float32
-		print "Loading as windows"
-		starttime = time()
-		X=[]
-		Y=[]
-		with open(filename) as f:
-			header = f.readlines(5)
-			for line in f:
-				x,y = line.strip().split(',')
-				X.append(x)
-				Y.append(y)
-		endtime = time()
-		print "Loaded data in %i seconds" %int(endtime - starttime)
-		return X,Y
+			#Count the number of lines in the file to be loaded.
+			from platform import system
+			from subprocess import check_output
+			system = system()
+			if system == "Windows":
+			    size = check_output("type %s | find /c /v '~~~'" %filename)
+			elif system == "Linux":
+				size = check_output(['wc','-l',filename])
+				size = size.split(' ')
 
-	if windows == True and (filename.endswith('.txt') or filename.endswith('.fft') or filename.endswith('.csv')):
-		x,y = winload(filename)
-	elif filename.endswith('.txt') or filename.endswith('.fft') or filename.endswith('.cvs'):
-		x,y = loadascii(filename)
-	elif filename.endswith('.trc') or filename.endswith('.raw'):
-		x,y = loadraw(filename)
-	else:
-		print "Failed to load data, returned zeros"
-		x = 0
-		y = 0
-	return x,y
+			#Make and populate and empty array with the data
+			from numpy import zeros
+			data = zeros((col,int(size[0])))
+			with open(filename) as f:
+				i=0
+				linedata = []
+				for line in f:
+					linedata = line.strip().split(',')
+					if len(linedata) >= 2 and isfloat(linedata):
+						for j in range(min(col,len(linedata))):
+							data[j][i] = linedata[j]
+						i+=1
+			data = data[:,:i]#wc will often over overestimate the of lines
+			endtime = time()
+			print "That took %i seconds" %int(endtime - starttime)
+			return data
+
+		def loadLeCroyRaw(filename):
+			startime = time()
+			import JLeCroy
+			print "Loading data from %s as raw" %filename
+			f = open(filename,'rb')
+			raw = f.read()
+			wave,t,x,Int = JLeCroy.InterpretWaveform(raw)
+			endtime = time()
+			print "That took %i seconds" %int(endtime - startime)
+			return t,x
+
+		from time import time
+		if filename.endswith('.trc') or filename.endswith('.raw'):
+			data = loadLeCroyRaw(filename)
+		else:
+			data = loadascii(filename)
+		return data
